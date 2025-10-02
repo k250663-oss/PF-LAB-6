6e53a6018fb99855db74b8f7eb2e1f6c5860e3b0
Mohaddisa Zehra <k250663@nu.edu.pk>
2025-10-02 13:22:33 +0500

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,3 +1,4 @@
+
 #include<stdio.h>
 int main(){
 	int a ,b ,mul=0 , div=0 , i ;
@@ -10,14 +11,14 @@ int main(){
 	for( i = 0 ; i < b ; i++){
 		mul += a;
 	}
-	int temp =a ;
 	
-	while(temp>= b)
-{
-	temp -= b ;
-	div++ ;
-}
-	printf("%d\n", mul);
-	printf("%f", div); 
+	
+	while(a >= b) {
+        a -= b ;
+        div++ ;
+    }
 
+    printf("%d\n", mul); 
+    printf("%d", div);   
+    return 0; 
 }
